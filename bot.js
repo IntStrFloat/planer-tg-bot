1a75475d17f7002d56eaf3a6e3e2b49b13dbb89c
IntStrFloat <108020874+IntStrFloat@users.noreply.github.com>
2023-08-27 00:15:02 +0300

    Add firebase

--- bot.js
+++ bot.js
@@ -1,8 +1,39 @@
-const {Telegraf} = require('telegraf')
+const {Telegraf,Markup} = require('telegraf')
+const { getFirestore, collection, getDocs,getDoc,addDoc,query,where } = require('firebase/firestore/lite');
 require('dotenv').config()
+const {db} = require('./db')
 const bot = new Telegraf(process.env.TOKEN)
-bot.command('start',()=>{
-    console.log('start')
+
+const tasksRef = collection(db,'tasks')
+
+bot.command('start',async (ctx)=>{
+    const data = await query(collection(db,'tasks'),where('user_id','==',ctx.chat.id))
+    const frf = await getDocs(data)
+    if(frf.docs.length===0) {
+        await addDoc(tasksRef,{
+            id:ctx.chat.id,
+            user_id:ctx.chat.id,
+            tasks:[]
+        })
+    }
+    ctx.reply('Привет, это простой планировщик задачь :).\nЧтобы добавить задачу, просто напиши её мне, а через пробел укажи время чтобы я мог тебе напомнить о задаче)\n(Формат - XX:XX)',Markup.keyboard([Markup.button.callback('Список дел','tasks')]))
+})
+
+bot.on('text',async (ctx)=>{
+    if (ctx.message.text === 'Список дел') {
+        const data = await query(collection(db,'tasks'),where('user_id','==',ctx.chat.id))
+        const frf = await getDocs(data)
+        console.log(frf.docs[0].data().tasks)
+        ctx.reply(`Дорогой, твой список дел: ${frf.docs[0].data().tasks.map((elem,index)=>{return `${index+1}) ${elem}\n`})}`)
+    } else {
+        
+        const data = await getDocs(tasksRef);
+        const filteredData = data.docs.map((doc)=>({...doc.data(),
+                                                        id: doc.id}))
+        console.log(filteredData[0])
+        // ctx.reply(`Есть, в твой список дел добавлена задача!\nТеперь он выглядит вот так: ${tasks[ctx.message.from.id].map((elem,index)=>{return `${index+1}) ${elem}\n`})}`);
+    }
+    
 })
 bot.launch().then(()=>{
     console.log('Бот запущен')
--- db.js
+++ db.js
@@ -1,5 +1,25 @@
-const {initializeApp } = require(' firebase/app ');
-const firebaseConfig = {
+// Import the functions you need from the SDKs you need
+const { initializeApp,firebase  } = require("firebase/app");
+const { getAnalytics } = require("firebase/analytics");
+const { getFirestore, collection, getDocs } = require('firebase/firestore/lite');
+require('dotenv').config()
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-}
-const app = initializeApp(firebaseConfig)
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+  }
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+module.exports = {db}
