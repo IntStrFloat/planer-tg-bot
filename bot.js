fce281fa0aa626b57d0b4c8aee7eed5ef19e04bb
IntStrFloat <108020874+IntStrFloat@users.noreply.github.com>
2023-08-27 19:05:22 +0300

    Saving tasks to firebase auto-recall every 7 hours

--- bot.js
+++ bot.js
@@ -1,36 +1,49 @@
 const {Telegraf,Markup} = require('telegraf')
-const { getFirestore, collection, getDocs,getDoc,addDoc,query,where } = require('firebase/firestore/lite');
+const { getFirestore, collection, getDocs,getDoc,addDoc,query,where, updateDoc,doc } = require('firebase/firestore/lite');
 require('dotenv').config()
 const {db} = require('./db')
 const bot = new Telegraf(process.env.TOKEN)
 
 const tasksRef = collection(db,'tasks')
-
-bot.command('start',async (ctx)=>{
+const sendTasks = async (ctx) =>{
     const data = await query(collection(db,'tasks'),where('user_id','==',ctx.chat.id))
-    const frf = await getDocs(data)
-    if(frf.docs.length===0) {
+    const documents = await getDocs(data)
+    return documents
+}
+bot.command('start',async (ctx)=>{
+    setInterval(async()=>{
+        const documents = await sendTasks(ctx);
+        console.log(documents.docs[0].data().tasks)
+        ctx.reply(`Дорогой, твой список дел: ${documents.docs[0].data().tasks.map((elem,index)=>{return `${index+1}) ${elem}\n`})}`)
+    },25200000)
+    const documents = await sendTasks(ctx);
+    if(documents.docs.length===0) {
         await addDoc(tasksRef,{
-            id:ctx.chat.id,
             user_id:ctx.chat.id,
             tasks:[]
         })
     }
     ctx.reply('Привет, это простой планировщик задачь :).\nЧтобы добавить задачу, просто напиши её мне, а через пробел укажи время чтобы я мог тебе напомнить о задаче)\n(Формат - XX:XX)',Markup.keyboard([Markup.button.callback('Список дел','tasks')]))
 })
-
 bot.on('text',async (ctx)=>{
     if (ctx.message.text === 'Список дел') {
-        const data = await query(collection(db,'tasks'),where('user_id','==',ctx.chat.id))
-        const frf = await getDocs(data)
-        console.log(frf.docs[0].data().tasks)
-        ctx.reply(`Дорогой, твой список дел: ${frf.docs[0].data().tasks.map((elem,index)=>{return `${index+1}) ${elem}\n`})}`)
+        const documents = await sendTasks(ctx);
+        console.log(documents.docs[0].data().tasks)
+        ctx.reply(`Дорогой, твой список дел: ${documents.docs[0].data().tasks.map((elem,index)=>{return `${index+1}) ${elem}\n`})}`)
     } else {
-        
-        const data = await getDocs(tasksRef);
-        const filteredData = data.docs.map((doc)=>({...doc.data(),
-                                                        id: doc.id}))
-        console.log(filteredData[0])
+        const documents = await sendTasks(ctx);
+        const docRef = doc(db,'tasks',documents.docs[0].id)
+        const data = {
+            tasks: [...documents.docs[0].data().tasks,ctx.message.text]
+        }
+        updateDoc(docRef,data)
+
+
+
+        // const data = await getDocs(tasksRef);
+        // const filteredData = data.docs.map((doc)=>({...doc.data(),
+        //                                                 id: doc.id}))
+        // console.log(filteredData[0])
         // ctx.reply(`Есть, в твой список дел добавлена задача!\nТеперь он выглядит вот так: ${tasks[ctx.message.from.id].map((elem,index)=>{return `${index+1}) ${elem}\n`})}`);
     }
     
